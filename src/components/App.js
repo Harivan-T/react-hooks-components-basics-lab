a5f1fd11f3eb737bdf2b06ba4615c4e176e73af9
Harivan-T <harivantahir@gmail.com>
2024-03-14 14:56:04 +0300

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -17,14 +17,24 @@ function Home() {
 }
 
 {/* write an <About> component here */}
-
+function About() {
+  return(
+   <div >
+      <h2 id="about"q>About</h2>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      {/* add the <Home> component here */
+      <Home/>}
+      
+      {/* add your <About> component here */
+        <About/>}
+    
     </div>
   );
 }
